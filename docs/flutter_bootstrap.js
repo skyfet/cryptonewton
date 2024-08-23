89dbcbb0d91ceaba2e9f387c7317f4f55381f150
skyfet <skryfet@ya.ru>
2024-08-23 23:47:49 +0300

    .

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1224899783"
+    serviceWorkerVersion: "1322613629"
   }
 });
