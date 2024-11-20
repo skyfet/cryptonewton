781d77297aa1091a61c9188e80e6361c31220d83
skyfet <skryfet@ya.ru>
2024-11-20 19:41:00 +0300

    .

--- android/app/src/main/kotlin/com/example/cryptonewton/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cryptonewton
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/screens/util/string_snackbar_extension.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension StrinExt on String {
-  void showSnackbar(BuildContext context) {
-    String message = this;
-    var snackBar = SnackBar(content: Text(message));
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-  }
-}
