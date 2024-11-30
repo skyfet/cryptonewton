292274a18239c8446da18147c0dffb4f22160b29
skyfet <skryfet@ya.ru>
2024-11-30 03:43:23 +0300

    feat: disable text selecting feat: better cursor mobile support

--- scripts.js
+++ scripts.js
@@ -1,14 +1,28 @@
 const cursor = document.getElementById("cursor");
 
+// Функция для обновления позиции курсора
+function updateCursorPosition(x, y) {
+  cursor.style.left = x + "px";
+  cursor.style.top = y + "px";
+}
+
+// Событие для мыши
 document.addEventListener("mousemove", (e) => {
-  cursor.style.left = e.clientX + "px"; // Используем clientX
-  cursor.style.top = e.clientY + "px"; // Используем clientY
+  updateCursorPosition(e.clientX, e.clientY);
 });
 
+// Событие для касания на мобильных устройствах
+document.addEventListener("touchmove", (e) => {
+  const touch = e.touches[0];
+  updateCursorPosition(touch.clientX, touch.clientY);
+});
+
+// Скрытие курсора при выходе мыши
 document.addEventListener("mouseleave", () => {
-  cursor.style.opacity = 0; // Скрыть курсор при выходе мыши
+  cursor.style.opacity = 0; // Скрыть курсор
 });
 
+// Показ курсора при возврате мыши
 document.addEventListener("mouseenter", () => {
-  cursor.style.opacity = 1; // Показать курсор при возвращении мыши
+  cursor.style.opacity = 1; // Показать курсор
 });
