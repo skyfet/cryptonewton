dc5ac7e99635cbe91c421b59015a60888573722e
skyfet <skryfet@ya.ru>
2025-07-03 19:09:49 +0300

    fix: focus amount input on build

--- frontend/src/components/AmountPicker.svelte
+++ frontend/src/components/AmountPicker.svelte
@@ -1,28 +1,52 @@
 <script>
   export let onSelect = () => {};
-  let custom = '';
+  let custom = "";
   let selected = null;
   const PRESETS = [50, 150, 250, 500, 1000, 2500];
 
   function choose(a) {
     selected = a;
-    custom = '';
+    custom = "";
     onSelect(a);
   }
+
+  let amountInput;
+
+  function focusAmountInput() {
+    amountInput.focus();
+  }
+
+  $: if (amountInput) focusAmountInput();
 </script>
 
 <div>
   <div class="amounts">
     {#each PRESETS as a}
-      <div class="amount {selected === a ? 'selected' : ''}" on:click={() => choose(a)}>{a}</div>
+      <div
+        class="amount {selected === a ? 'selected' : ''}"
+        on:click={() => choose(a)}
+      >
+        {a}
+      </div>
     {/each}
-    {#if selected != 'custom'}
-      <div class="amount {selected === 'custom' ? 'selected' : ''}" on:click={() => choose('custom')}>Ввести</div>
+    {#if selected != "custom"}
+      <div
+        class="amount {selected === 'custom' ? 'selected' : ''}"
+        on:click={() => choose("custom")}
+      >
+        Ввести
+      </div>
     {/if}
-    {#if selected === 'custom'}
-      <input class="amount {selected === 'custom' ? 'selected' : ''}" type="number" bind:value={custom} on:input={() => onSelect(parseInt(custom) || 0)} min="1" max="250000" />
+    {#if selected === "custom"}
+      <input
+        bind:this={amountInput}
+        class="amount {selected === 'custom' ? 'selected' : ''}"
+        type="number"
+        bind:value={custom}
+        on:input={() => onSelect(parseInt(custom) || 0)}
+        min="1"
+        max="250000"
+      />
     {/if}
-
   </div>
-  
 </div>
