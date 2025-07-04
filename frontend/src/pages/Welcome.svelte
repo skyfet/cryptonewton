903c5357103532e7285050acb6cf073b9bbd9583
archi d bill <skryfet@ya.ru>
2025-07-04 17:55:43 +0300

    !

--- frontend/src/pages/Welcome.svelte
+++ frontend/src/pages/Welcome.svelte
@@ -2,13 +2,28 @@
   import { navigate } from "../router.js";
   import History from "./History.svelte";
   import RippleButton from "../components/RippleButton.svelte";
+  import Meteors from "../components/Meteors.svelte";
 </script>
 
 <div class="container">
-  <h1 class="header fancy-text">Star ⭐ Pay Terminal</h1>
+  <div
+    class="relative flex h-fit w-full max-w-lg items-center justify-center overflow-hidden rounded-lg border bg-background p-20 md:shadow-xl"
+  >
+    <Meteors number={30} />
+    <p
+      class="z-10 whitespace-pre-wrap text-center text-5xl tracking-tight font-semibold bg-gradient-to-br from-white to-blue-900/40 text-transparent bg-clip-text"
+    >
+      Star ⭐ Pay Terminal
+    </p>
+  </div>
+
   <div class="button-group">
-    <RippleButton onClick={() => navigate("/buy")}>🌟 Купить звёзды</RippleButton>
-    <RippleButton onClick={() => navigate("/buy")}>🎁 Подарить звёзды</RippleButton>
+    <RippleButton onClick={() => navigate("/buy")}
+      >🌟 Купить звёзды</RippleButton
+    >
+    <RippleButton onClick={() => navigate("/buy")}
+      >🎁 Подарить звёзды</RippleButton
+    >
   </div>
   <History></History>
 </div>
