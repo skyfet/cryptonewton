c1a84562c6bbb0d2fdab015d6fb44e55f0276020
Artem <45742058+skyfet@users.noreply.github.com>
2025-07-06 08:39:12 +0300

    Add contact flow

--- bot/bot.js
+++ bot/bot.js
@@ -1,13 +1,15 @@
 const TelegramBot = require('node-telegram-bot-api');
 const menuHandler = require('./handlers/menuHandler');
 const inlineQueryHandler = require('./handlers/inlineQueryHandler');
+const contactHandler = require('./handlers/contactHandler');
 const app = require('./server');
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/start/, (msg) => menuHandler(bot, msg));
-bot.on('inline_query', (query) => inlineQueryHandler(bot, query));
+bot.onText(/\/start/, msg => menuHandler(bot, msg));
+bot.on('contact', msg => contactHandler(bot, msg));
+bot.on('inline_query', query => inlineQueryHandler(bot, query));
 
 if (require.main === module) {
   const port = process.env.PORT || 3000;
