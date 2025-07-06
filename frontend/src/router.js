0574bfa3071c76711b1bd3b1ac68ccb8c5b462c4
Artem <45742058+skyfet@users.noreply.github.com>
2025-07-06 09:25:46 +0300

    Handle query params in hash

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,7 +1,27 @@
 import { writable } from 'svelte/store'
 
 function getPath() {
-  const hash = window.location.hash.slice(1)
+  let hash = window.location.hash.slice(1)
+
+  // move query params from hash to search
+  const idx = hash.indexOf('?')
+  if (idx !== -1) {
+    const query = hash.slice(idx + 1)
+    hash = hash.slice(0, idx)
+    const params = new URLSearchParams(window.location.search)
+    for (const [k, v] of new URLSearchParams(query)) {
+      params.set(k, v)
+    }
+    const search = params.toString()
+    window.history.replaceState(
+      null,
+      '',
+      `${window.location.pathname}${search ? `?${search}` : ''}#${hash}`
+    )
+  }
+
+  if (hash.endsWith('/')) hash = hash.slice(0, -1)
+
   if (hash) return hash
   const params = new URLSearchParams(window.location.search)
   if (params.get('mode') === 'gift' || params.has('user_id')) {
