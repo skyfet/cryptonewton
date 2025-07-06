a9a8c9388e935d577f6fe204a9d30a751cc9ad77
archi d bill <skryfet@ya.ru>
2025-07-06 08:08:11 +0300

    fix and deploy

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 // main.js (или main.ts)
 import App from './App.svelte';
 import { mount } from 'svelte';
+import './index.css';
 
 const app = mount(App, { target: document.getElementById("root") });
 
